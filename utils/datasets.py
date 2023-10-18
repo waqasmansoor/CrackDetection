ce5a1aba6245719f8cb7082a4f727cb05a57b47d
Mustafa Mansoor <mustafamansoor47@gmail.com>
2023-10-18 01:24:59 -0400

    print

--- utils/datasets.py
+++ utils/datasets.py
@@ -1235,6 +1235,7 @@ class Albumentations:
 
     def __call__(self, im, labels, p=1.0):
         if self.transform and random.random() < p:
+            print("albumentation..................")
             new = self.transform(image=im, bboxes=labels[:, 1:], class_labels=labels[:, 0])  # transformed
             im, labels = new['image'], np.array([[c, *b] for c, b in zip(new['class_labels'], new['bboxes'])])
         return im, labels
