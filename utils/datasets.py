7f348b218563f5e1eae2263f58514f3aa905be44
Mustafa Mansoor <mustafamansoor47@gmail.com>
2023-10-21 01:05:57 -0400

    turned off blur

--- utils/datasets.py
+++ utils/datasets.py
@@ -1225,10 +1225,11 @@ class Albumentations:
             A.CLAHE(p=0.01),
             A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.01),
             A.RandomGamma(gamma_limit=[80, 120], p=0.01),
-            A.Blur(p=0.01),
-            A.MedianBlur(p=0.01),
+            # A.Blur(p=0.01),
+            # A.MedianBlur(p=0.01),
             A.ToGray(p=0.01),
-            A.ImageCompression(quality_lower=75, p=0.01),],
+            # A.ImageCompression(quality_lower=75, p=0.01),
+            ],
             bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
 
             #logging.info(colorstr('albumentations: ') + ', '.join(f'{x}' for x in self.transform.transforms if x.p))
